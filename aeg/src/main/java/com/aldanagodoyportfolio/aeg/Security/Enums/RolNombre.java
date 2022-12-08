c60c2b7f554376352d38b72ad17c6a524e23788d
Aldana Godoy <godoyaldana@gmail.com>
2022-12-07 21:45:02 -0300

    agragando dependencias y RolNombre

---
+++ aeg/src/main/java/com/aldanagodoyportfolio/aeg/Security/Enums/RolNombre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aldanagodoyportfolio.aeg.Security.Enums;
+
+/**
+ *
+ * @author Godoy
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
